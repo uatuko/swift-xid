9557226ddce16deabcf33beaa69497d061040188
Uditha Atukorala <ua@nuked.zone>
2022-06-13 16:00:00 +0100

    test cases

--- src/XidError.swift
+++ src/XidError.swift
@@ -1,4 +1,4 @@
-enum XidError: Error {
+enum XidError: Error, Equatable {
 	case decodeValidationFailure
 	case invalidId
 	case invalidIdStringLength(have: Int, want: Int)
--- test/xidTests.swift
+++ test/xidTests.swift
@@ -22,6 +22,22 @@ final class xidTests: XCTestCase {
 		XCTAssertEqual(expected, actual)
 	}
 
+	func testIdInitFromDataThrow() {
+		XCTAssertThrowsError(try Id(from:Data([0x78, 0x69, 0x64]))) { error in
+			XCTAssertEqual(XidError.invalidId, error as! XidError)
+		}
+	}
+
+	func testIdInitFromStringThrow() {
+		XCTAssertThrowsError(try Id(from: "xid")) { error in
+			XCTAssertEqual(XidError.invalidIdStringLength(have: 3, want: 20), error as! XidError)
+		}
+
+		XCTAssertThrowsError(try Id(from: "caia5ng890f0tr00hgt=")) { error in
+			XCTAssertEqual(XidError.decodeValidationFailure, error as! XidError)
+		}
+	}
+
 	func testIdPartsExtraction() {
 		struct Test {
 			var id: Id
@@ -70,6 +86,63 @@ final class xidTests: XCTestCase {
 		XCTAssertEqual("9m4e2mr0ui3e8a215n4g", String(describing: id))
 	}
 
+	func testJsonDecode() throws {
+		struct User: Decodable {
+			var id: Id
+			var name: String
+		}
+
+		let data = """
+		{
+		  "id": "cajj5ctbutokmmm2jdsg",
+		  "name": "user:testJsonDecode"
+		}
+		""".data(using: .utf8)!
+
+		let decoder = JSONDecoder()
+		let user = try decoder.decode(User.self, from: data)
+
+		XCTAssertEqual("cajj5ctbutokmmm2jdsg", String(describing: user.id))
+	}
+
+	func testJsonEncode() throws {
+		struct User: Encodable {
+			var id: Id
+			var name: String
+		}
+
+		let user = User(id: NewXid(), name: "user:testJsonEncode")
+
+		let encoder = JSONEncoder()
+		let data = try encoder.encode(user)
+
+		print(String(data: data, encoding: .utf8)!)
+		XCTAssertEqual(
+			"{\"id\":\"\(user.id)\",\"name\":\"user:testJsonEncode\"}",
+			String(data: data, encoding: .utf8)
+		)
+	}
+
+	func testNewXidFromBytes() throws {
+		let id = try NewXid(bytes: Data([0x62, 0xa7, 0x28, 0x66, 0xab, 0xf7, 0x71, 0x46, 0x09, 0xa4, 0xa3, 0x55]))
+
+		XCTAssertEqual("cajigplbutokc2d4kdag", String(describing: id))
+	}
+
+	func testNewXidFromData() throws {
+		let actual = try NewXid(from: "9m4e2mr0ui3e8a215n4g".data(using: .utf8)!)
+		let expected = Id(bytes: Data([0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9]))
+
+		XCTAssertEqual(expected, actual)
+	}
+
+	func testNewXidFromString() throws {
+		let actual = try NewXid(from: "9m4e2mr0ui3e8a215n4g")
+		let expected = Id(bytes: Data([0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9]))
+
+		XCTAssertEqual(expected, actual)
+	}
+
 	func testXidNext() {
 		let n = 100
 		var xid = Xid()
@@ -108,5 +181,10 @@ final class xidTests: XCTestCase {
 			let diff = current.counter() - previous.counter()
 			XCTAssertEqual(diff, 1)
 		}
+	}
+
+	func testSomething() {
+//		let id: String = NewXid()
+		let id: Id = NewXid()
 	}
 }
