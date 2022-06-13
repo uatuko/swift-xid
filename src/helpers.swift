98987e9c7d526d0cab56b635d68706a445e3fd68
Uditha Atukorala <ua@nuked.zone>
2022-06-13 15:59:33 +0100

    more convenience functions

--- src/helpers.swift
+++ src/helpers.swift
@@ -1,5 +1,27 @@
+import Foundation
+
 private var xid = Xid()
+
+public func NewXid() -> Id {
+	xid.next()
+}
 
 public func NewXid() -> String {
 	String(describing: xid.next())
+}
+
+public func NewXid(bytes: Data) throws -> Id {
+	if bytes.count != 12 {
+		throw XidError.invalidId
+	}
+
+	return Id(bytes: bytes)
+}
+
+public func NewXid(from: Data) throws -> Id {
+	try Id(from: from)
+}
+
+public func NewXid(from: String) throws -> Id {
+	try Id(from: from)
 }
